11a9645b1adc181f6ad780ba21cda8075031b13b
viniciusram2025 <vinicius.ram2025@gmail.com>
2023-05-16 18:02:41 -0300

    Inicio do Projeto de components reusaveis

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        "extensions": [".js", ".jsx"]
+      }
+    ],
+    "react/react-in-jsx-scope": "off",
+    "comma-dangle": "off",
+    "import/prefer-default-export": "off",
+  },
+};
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+export function Navbar() {
+    return (
+        <S.Container>
+          <h1>Home</h1>
+        </S.Container>
+    )
+}
